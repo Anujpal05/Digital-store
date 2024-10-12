d3b322c40ffac8fd37514ae829d082a9534b1e6f
Anujpal05 <ap091453@gmail.com>
2024-10-12 18:00:30 +0530

    Some changes

--- server/Router/paymentRouter.js
+++ server/Router/paymentRouter.js
@@ -1,15 +1,8 @@
 import express from "express";
-import {
-  checkout,
-  getSession,
-  handleWebHook,
-  verifyPaymentStatus,
-} from "../Controller/paymentController.js";
+import { checkout } from "../Controller/paymentController.js";
 
 const router = express.Router();
 
 router.route("/checkout").post(checkout);
-router.route("/verify").post(verifyPaymentStatus);
-router.route("/session/:sessionId").get(getSession);
 
 export default router;
